a3b256d35d5d161217034ed7f77fda3685af9c65
Ganesh <adhimulamganesh935@gmail.com>
2026-09-17 09:45:50 +0530

    Delete UNIT-2-20260910T040842Z-1-001 directory

--- UNIT-2-20260910T040842Z-1-001/UNIT-2/week6/FindTheIndexOfTheFirstOccurrenceInAString.java
+++
@@ -1,8 +0,0 @@
-public class FindTheIndexOfTheFirstOccurrenceInAString {
-    public int strStr(String haystack, String needle) {
-        return haystack.indexOf(needle);
-    }
-    public static void main(String[] args) {
-        System.out.println(new FindTheIndexOfTheFirstOccurrenceInAString().strStr("sadbutsad", "sad"));
-    }
-}
--- UNIT-2-20260910T040842Z-1-001/UNIT-2/week6/RotateString.java
+++
@@ -1,8 +0,0 @@
-public class RotateString {
-    public boolean rotateString(String s, String goal) {
-        return s.length() == goal.length() && (s + s).contains(goal);
-    }
-    public static void main(String[] args) {
-        System.out.println(new RotateString().rotateString("abcde", "cdeab"));
-    }
-}
